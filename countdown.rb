9b4b63e73eb723754d114784ebda770b099181e7
Yuriy Ivanenko <yuriy.ivanenko@powerhrg.com>
2024-07-11 11:25:25 -0400

    Pass All Tests

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,20 @@
 #write your code here
 
-def countdown
+def countdown(int)
+  x = int
+  while x > 0
+    puts "#{x} SECOND(S)!"
+    x -= 1
+  end 
+  "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(int)
+  x = int
+  while x > 0
+    puts "#{x} SECOND(S)!"
+    x -= 1
+    sleep(1)
+  end 
+  "HAPPY NEW YEAR!"
 end
